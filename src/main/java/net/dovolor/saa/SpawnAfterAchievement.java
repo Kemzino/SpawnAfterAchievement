f6f7bd5eb19535231c4e4f6443bcec3060685901
IhorDovh <holdargraf@gmail.com>
2024-09-04 20:03:05 +0300

    few fixes

--- src/main/java/net/dovolor/saa/SpawnAfterAchievement.java
+++ src/main/java/net/dovolor/saa/SpawnAfterAchievement.java
@@ -2,29 +2,22 @@ package net.dovolor.saa;
 
 import net.dovolor.saa.config.EntityBlockConfig;
 import net.dovolor.saa.config.EntitySpawnConfig;
-import net.dovolor.saa.manager.AchivementManager;
 import net.dovolor.saa.manager.EntityManager;
-import net.dovolor.saa.mixin.AdvancementTrackerMixin;
 import net.dovolor.saa.store.CustomAdvancementStorage;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.advancement.PlayerAdvancementTracker;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.Registries;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.rmi.registry.Registry;
 
 
 public class SpawnAfterAchievement implements ModInitializer {
